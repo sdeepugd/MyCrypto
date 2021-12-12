0a815f475d67e765d6d3b83d4ff14330e41046e6
sdeepugd <sdeepugd@gmail.com>
2021-12-12 23:30:01 +0530

    Home screen done

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 import React from 'react'
+import styles from './style'
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.root}>
+            <Image style={styles.image} source={require('../../assets/images/Saly-1.png')} />
+            <Text style={styles.header1}>Welcome to MyCrypto</Text>
+            <Text style={styles.header2}>Invest your virtual $100.000 and compete with others</Text>
         </View>
     )
 }
---
+++ src/screens/Home/style.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+    root:{
+        padding: 10,
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    image:{
+        height: '40%',
+        aspectRatio: 1,
+    },
+    header1:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 10,
+    },
+    header2:{
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    }
+});
+export default styles;
